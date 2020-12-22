3a20ae2ef95106afd9a42ec4ac65341ef4289a4f
Moritz <moritz.banhardt@web.de>
2020-12-22 16:09:17 +0100

    pie chart

--- ki-site/src/main.js
+++ ki-site/src/main.js
@@ -5,10 +5,14 @@ import App from './App.vue'
 import 'fullpage.js/vendors/scrolloverflow' // Optional. When using scrollOverflow:true
 import VueFullPage from 'vue-fullpage.js'
 
+// Import Highcharts
+import HighchartsVue from 'highcharts-vue'
+
 
 Vue.config.productionTip = false
 
 Vue.use(VueFullPage);
+Vue.use(HighchartsVue);
 
 new Vue({
   el: '#app',
