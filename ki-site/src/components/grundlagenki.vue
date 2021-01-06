73a5c1539d6e7304e52fa85d0cd95625a5851927
Fabienne <Fabi>
2021-01-06 17:39:12 +0100

    Farbe Legende und Axen BedeutungKI

--- ki-site/src/components/grundlagenki.vue
+++ ki-site/src/components/grundlagenki.vue
@@ -1,9 +1,23 @@
 <template>
     <div id="glossar-contentID" class="site-container glossar-content">
-      <h2>Grundlagen zu KI</h2>
-        <p>kurze Erläuterung zu wichtigen Begriffen (z.B. KI allgemein) </p>
+      <h3>Grundlagen zu KI</h3>
+          <p>Dieses Glossar beinhaltet sämtliche Definitionen und Hintergrundinformationen zu den Themengebieten Künstliche Intelligenz. Diese Grundlagen sollten verstanden werden, damit die auf dieser Website dargestellten Grafiken nachvollziehbar sind.
+          Es ist jederzeit möglich, zurück zu dieser Seite zu springen, damit Infos nachgelesen werden können.
+          </p>
         <br>
         <div>
+
+
+            <h3> Definitionen </h3>
+              <h2> Künstliche Intelligenz </h2>
+                <p> Künstliche Intelligenz (KI) zählt heutzutage zu den entscheidendsten Schlüsseltechnologien. KI ermöglicht die Simulation von menschlicher Intelligenz, dazu gehört neben dem Lernen der Erfassung von Informationen die Schlussfolgerung, also wie wird mit Informationen umgegangen, um Rückschlüsse zu ziehen, sowie die Selbstkorrektur als abschließender Prozesspunkt.  <br>
+                    <br> Beispiele für den KI-Einsatz in Unternehmen:
+                    <br>Im Gesundheitswesen: Für Patienten ist es möglich, Fragen an einen Chatbot zu stellen. Dieser hat Zugriff auf Patientendaten und kann auf große Datenmengen zugreifen, damit Patienten auch von zu Hause aus beraten werden können.
+                    <br>Im Finanzwesen: Auch finanzielle Beratung kann via Chatbots von Kunden eingeholt werden. 
+                    <br>In der Bildung: Benotungen können automatisiert von einer KI übernommen werden, damit den Lehrkräften mehr Zeit für die Betreuung der Schüler bleibt. 
+                    <br>In der Fertigung: Der Einsatz von Robotern kann den Workflow vieler Prozesse verschnellern.
+                </p>
+
             <h3>Abkürzungen</h3>
             <table cellspacing="20" class="tbl">
                 <tr>
@@ -20,14 +34,6 @@
                     </td>
                     <td class="tabellentext">
                         Forschung und Entwicklung
-                    </td>               
-                </tr>
-                <tr>
-                    <td class="fett tabellentext">
-                        ...
-                    </td>
-                    <td class="tabellentext">
-                        ...
                     </td>               
                 </tr>
             </table>
