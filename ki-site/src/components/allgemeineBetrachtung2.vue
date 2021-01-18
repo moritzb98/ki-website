416ba0bb36797377ef43b724c8c20b2a6683b4d9
Theresa <theresabrenner@gmx.net>
2021-01-18 15:03:42 +0100

    Säulendiagramm in Balkendiagramm umgewandelt

--- ki-site/src/components/allgemeineBetrachtung2.vue
+++ ki-site/src/components/allgemeineBetrachtung2.vue
@@ -32,8 +32,62 @@ export default {
   },
   data() {
     return {
+        chartOptions: {
+             chart: {
+                type: 'bar',
+                styledMode: false,
+                // Hintergrundfarbe
+                backgroundColor: "none", 
+                width:800
+            },
+            title: {
+                text: ''
+            },
+
+            xAxis: {
+                categories: ['Africa', 'America', 'Asia', 'Europe', 'Oceania'],
+                title: {
+                    text: null
+                }
+            },
+             yAxis: [{
+            min: 0,
+            title: {
+                text: 'Total fruit consumption'
+            }
+        },{
+            linkedTo: 0,
+            opposite: true
+        }],
+            tooltip: {
+                valueSuffix: ' millions'
+            },
+            plotOptions: {
+                bar: {
+                    dataLabels: {
+                        enabled: true
+                    }
+                }
+            },
+            credits: {
+                enabled: false
+            },
+            series: [{
+                name: 'Year 1800',
+                data: [107, 31, 635, 203, 2]
+            }, {
+                name: 'Year 1900',
+                data: [133, 156, 947, 408, 6]
+            }, {
+                name: 'Year 2000',
+                data: [814, 841, 3714, 727, 31]
+            }, {
+                name: 'Year 2016',
+                data: [1216, 1001, 4436, 738, 40]
+            }]
+        },
     // Variable von html Element
-      chartOptions: {
+        chartOptions2: {
             chart: {
                 type: 'column',
                 styledMode: false,
