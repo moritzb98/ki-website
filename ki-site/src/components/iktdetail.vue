ebdd90c550917b872e80e6826266407ba12cf7d9
Moritz <moritz.banhardt@web.de>
2021-01-22 14:13:33 +0100

    company interaction

--- ki-site/src/components/iktdetail.vue
+++ ki-site/src/components/iktdetail.vue
@@ -39,7 +39,115 @@
          </div>
          <div class="slide" id="slide2">
             <div class="slide-container">
-              <img class="bild" src="../assets/Firma_alle_Daten.svg">
+            <!-- SVG ALLE DATEN -->
+              <svg id="firma-svg" data-name="Ebene 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 297 210">
+              <title>Firma</title>
+                <g>
+                  <rect class="cls-1" x="16.08" y="152.25" width="4.71" height="6.35"/>
+                  <path class="cls-2" d="M11.31,152.25H25.55a.72.72,0,0,0,.7-.88l-7.14-32.66a.7.7,0,0,0-1.36,0l-7.14,32.66A.72.72,0,0,0,11.31,152.25Z"/>
+                </g>
+                <g>
+                  <rect class="cls-1" x="269.29" y="151.25" width="4.71" height="6.35"/>
+                  <path class="cls-2" d="M264.53,151.25h14.23a.72.72,0,0,0,.7-.88l-7.14-32.66a.7.7,0,0,0-1.36,0l-7.14,32.66A.72.72,0,0,0,264.53,151.25Z"/>
+                </g>
+                <rect class="cls-3" x="22.72" y="123.43" width="175.47" height="34.27"/>
+                <rect class="cls-4" x="70.72" y="118.75" width="127.46" height="4.67"/>
+                <rect class="cls-3" x="65.12" y="72.54" width="124.55" height="18.43"/>
+                <g>
+                  <rect class="cls-5" x="70.72" y="94.36" width="118.94" height="10.64"/>
+                  <line class="cls-6" x1="89.29" y1="105" x2="89.29" y2="94.36"/>
+                  <polyline class="cls-6" points="107.12 94.36 114.53 94.36 114.53 105"/>
+                  <line class="cls-6" x1="140.01" y1="105" x2="140.01" y2="94.36"/>
+                  <line class="cls-6" x1="164.87" y1="105" x2="164.87" y2="94.36"/>
+                </g>
+                <g>
+                  <rect class="cls-5" x="117.85" y="128.71" width="75.13" height="16.05"/>
+                  <line class="cls-6" x1="143.33" y1="144.11" x2="143.33" y2="128.06"/>
+                  <line class="cls-6" x1="168.19" y1="145.4" x2="168.19" y2="129.35"/>
+                </g>
+                <g>
+                  <rect class="cls-5" x="25.56" y="128.07" width="50.34" height="16.05"/>
+                  <line class="cls-6" x1="42.63" y1="143.46" x2="42.63" y2="127.41"/>
+                  <line class="cls-6" x1="59.29" y1="144.75" x2="59.29" y2="128.71"/>
+                </g>
+                <rect class="cls-3" x="189.66" y="54.13" width="82.98" height="8.54"/>
+                <g>
+                  <rect class="cls-7" x="96.89" y="128.07" width="15.1" height="29.33"/>
+                  <rect class="cls-7" x="80.91" y="128.07" width="15.1" height="29.33"/>
+                </g>
+                <rect class="cls-8" x="79.62" y="126.87" width="33.58" height="32.26"/>
+                <g>
+                  <rect class="cls-9" x="8.59" y="157.7" width="266.81" height="3.93"/>
+                  <line class="cls-10" x1="9.94" y1="157.7" x2="9.94" y2="161.63"/>
+                  <line class="cls-10" x1="36.51" y1="157.7" x2="36.51" y2="161.63"/>
+                  <line class="cls-10" x1="63.12" y1="157.7" x2="63.12" y2="161.63"/>
+                  <line class="cls-10" x1="89.29" y1="157.7" x2="89.29" y2="161.63"/>
+                  <line class="cls-10" x1="115.45" y1="157.7" x2="115.45" y2="161.63"/>
+                  <line class="cls-10" x1="141.91" y1="157.7" x2="141.91" y2="161.63"/>
+                  <line class="cls-10" x1="168.65" y1="157.7" x2="168.65" y2="161.63"/>
+                  <line class="cls-10" x1="194.78" y1="157.7" x2="194.78" y2="161.63"/>
+                  <line class="cls-10" x1="221.26" y1="157.7" x2="221.26" y2="161.63"/>
+                  <line class="cls-10" x1="247.74" y1="157.7" x2="247.74" y2="161.63"/>
+                  <line class="cls-10" x1="274.65" y1="157.7" x2="274.65" y2="161.63"/>
+                </g>
+                <rect class="cls-11" x="244.52" y="62.67" width="21.67" height="95.02"/>
+                <rect class="cls-11" x="196.19" y="62.57" width="21.67" height="95.02"/>
+                <polygon class="cls-12" points="65.06 113.85 197.86 113.85 196.34 118.75 70.71 118.75 65.06 113.85"/>
+                <rect class="cls-3" x="189.36" y="54.13" width="8.5" height="50.96"/>
+                <rect class="cls-3" x="65.06" y="104.45" width="132.8" height="9.4"/>
+                <polygon class="cls-13" points="266.19 62.67 272.64 62.67 266.19 70.25 266.19 62.67"/>
+                <rect class="cls-13" x="217.37" y="62.57" width="27.69" height="7.69"/>
+                <g>
+                  <rect class="cls-5" x="187.52" y="100.59" width="87.34" height="26.66" transform="translate(345.11 -117.26) rotate(90)"/>
+                  <line class="cls-6" x1="217.88" y1="99.92" x2="244.54" y2="99.92"/>
+                  <line class="cls-6" x1="217.82" y1="129.35" x2="244.48" y2="129.35"/>
+                </g>
+                <polygon class="cls-14" points="196.19 157.59 198.19 157.7 197.86 113.85 196.25 118.83 196.19 157.59"/>
+                <polygon class="cls-15" points="231.15 70.25 244.52 70.25 244.48 86.33 231.15 70.25"/>
+                <polygon class="cls-15" points="105.67 94.51 114.27 94.51 114.25 104.85 111.6 104.85 105.67 94.51"/>
+                <polygon class="cls-15" points="180.01 129.16 192.98 129.16 192.95 144.76 189.96 144.76 180.01 129.16"/>
+                <polygon class="cls-15" points="97.07 128.44 111.81 128.44 111.78 148.84 97.07 130.96 97.07 128.44"/>
+                <polygon class="cls-15" points="223.48 99.92 244.52 99.92 244.46 125.64 223.48 99.92"/>
+                <polygon class="cls-15" points="122.89 104.85 114.29 104.85 114.31 94.51 116.96 94.51 122.89 104.85"/>
+                <polygon class="cls-15" points="37.95 143.79 26.12 143.79 26.15 128.39 29.79 128.39 37.95 143.79"/>
+                <polygon class="cls-15" points="156.26 94.21 164.86 94.21 164.84 104.55 162.19 104.55 156.26 94.21"/>
+                <polygon class="cls-15" points="173.48 104.55 164.88 104.55 164.9 94.21 167.55 94.21 173.48 104.55"/>
+                <polygon class="cls-15" points="151.8 94.58 154.54 94.58 160.56 104.78 158.1 104.78 151.8 94.58"/>
+                <polygon class="cls-15" points="121.28 129.16 126.44 129.16 137.8 145.59 133.16 145.59 121.28 129.16"/>
+                <polygon class="cls-16" points="266.19 68.99 266.19 156.43 20.78 157.32 22.71 122.16 70.72 122.16 70.71 117.49 65.06 112.59 65.06 103.18 70.72 103.73 70.72 93.09 65.12 89.57 65.12 71.27 189.66 71.27 189.36 52.87 272.64 52.87 272.64 61.41 266.19 68.99"/>
+                <rect class="cls-17" x="203.01" y="76.4" width="57.13" height="34.59"/>
+                <rect class="cls-17" x="203.01" y="118.23" width="57.13" height="34.59"/>
+                <rect class="cls-17" x="139.43" y="76.27" width="57.13" height="34.59"/>
+                <rect class="cls-17" x="139.43" y="118.1" width="57.13" height="34.59"/>
+                <rect class="cls-17" x="75.9" y="76.27" width="57.13" height="34.59"/>
+                <rect class="cls-17" x="75.9" y="118.1" width="57.13" height="34.59"/>
+                <text class="cls-18" transform="translate(81.5 105.54)">57%</text>
+                <text class="cls-19" transform="translate(112.33 105.54)">52%</text>
+                <text class="cls-20" transform="translate(89.04 87.61)">P<tspan class="cls-21" x="3.81" y="0">r</tspan><tspan x="5.86" y="0">oduktion, </tspan><tspan x="-7.34" y="7.2">Dienstleistungen</tspan></text>
+                <text class="cls-18" transform="translate(145.1 105.71)">14%</text>
+                <text class="cls-19" transform="translate(175.94 105.71)">37%</text>
+                <text class="cls-20" transform="translate(148.1 87.78)">Administration</text>
+                <text class="cls-18" transform="translate(208.68 105.07)">8%</text>
+                <text class="cls-19" transform="translate(242.52 105.07)">5%</text>
+                <text class="cls-20" transform="translate(206.11 87.13)"><tspan class="cls-22">V</tspan><tspan x="3.67" y="0">e</tspan><tspan class="cls-23" x="6.81" y="0">r</tspan><tspan x="8.96" y="0">trieb, Mar</tspan><tspan class="cls-21" x="34.43" y="0">k</tspan><tspan x="37.44" y="0">eting</tspan></text>
+                <text class="cls-18" transform="translate(81.5 147.59)">36%</text>
+                <text class="cls-19" transform="translate(112.33 147.59)">21%</text>
+                <text class="cls-20" transform="translate(83.82 129.66)"><tspan class="cls-24">F</tspan><tspan x="3.25" y="0">uE, Inn</tspan><tspan class="cls-25" x="21.16" y="0">ov</tspan><tspan x="27.5" y="0">ation</tspan></text>
+                <text class="cls-18" transform="translate(145.1 147.59)">26%</text>
+                <text class="cls-19" transform="translate(175.94 147.59)">20%</text>
+                <text class="cls-20" transform="translate(140.83 129.65)">Informationstechnik</text>
+                <text class="cls-18" transform="translate(208.68 147.59)">4%</text>
+                <text class="cls-19" transform="translate(242.52 147.59)">3%</text>
+                <text class="cls-20" transform="translate(209.65 129.65)">Einkauf/Logistik</text>
+                <g>
+                  <path v-on:click="disableIkt" class="cls-26 button" d="M151.39,180a3.52,3.52,0,0,0-3.52,3.52v.94a3.51,3.51,0,0,0,3.52,3.51h8.44v-8Z"/>
+                  <path v-on:click="disableGesamt" class="cls-27 button" d="M169.12,180h-8.44v8h8.44a3.51,3.51,0,0,0,3.52-3.51v-.94A3.52,3.52,0,0,0,169.12,180Z"/>
+                </g>
+                <text class="cls-28" transform="translate(133.69 186.1)">IKT</text>
+                <text class="cls-28" transform="translate(177.72 186.1)">Gesamtwi<tspan class="cls-23" x="26.96" y="0">r</tspan><tspan x="29.11" y="0">tschaft</tspan></text>
+              </svg>
+
+    
             </div>
          </div>
          <div class="slide">
@@ -60,6 +168,22 @@ export default {
 
     }
   },
+  methods:{
+    disableGesamt: function(){
+      var el = document.getElementsByClassName('cls-19');
+      for(var i = 0; i<el.length; i++){
+        el[i].classList.toggle('hide');
+      }
+      document.getElementsByClassName('cls-27')[0].classList.toggle('disable');
+    },
+    disableIkt: function(){
+      var el = document.getElementsByClassName('cls-18');
+      for(var i = 0; i<el.length; i++){
+        el[i].classList.toggle('hide');
+      }
+      document.getElementsByClassName('cls-26')[0].classList.toggle('disable');
+    }
+  }
 
 }
 </script>
@@ -142,5 +266,161 @@ video{
 .site-container{
   height: 100%;
 }
+
+.cls-1 {
+  fill: #523e14;
+}
+
+.cls-2 {
+  fill: #9fb946;
+  stroke: #91a23e;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+}
+
+.cls-15, .cls-17, .cls-3 {
+  fill: #e4eaea;
+}
+
+.cls-12, .cls-4 {
+  fill: #a5bcca;
+}
+
+.cls-4 {
+  opacity: 0.68;
+}
+
+.cls-5 {
+  fill: #1eace2;
+}
+
+.cls-5, .cls-6 {
+  stroke: #e8e9ea;
+}
+
+.cls-10, .cls-16, .cls-17, .cls-5, .cls-6, .cls-7, .cls-8 {
+  stroke-miterlimit: 10;
+}
+
+.cls-10, .cls-6, .cls-8 {
+  fill: none;
+}
+
+.cls-7 {
+  fill: #29a8e1;
+  stroke: #dbe1e2;
+}
+
+.cls-8 {
+  stroke: #9bafb8;
+  stroke-width: 1.5px;
+}
+
+.cls-9 {
+  fill: #d5e0e7;
+}
+
+.cls-10 {
+  stroke: #bbc4c9;
+}
+
+.cls-11 {
+  fill: #4c6088;
+}
+
+.cls-13 {
+  fill: #9bacbb;
+}
+
+.cls-14 {
+  fill: #2e415c;
+}
+
+.cls-15 {
+  opacity: 0.4;
+}
+
+.cls-16 {
+  fill: #969696;
+}
+
+.cls-16, .cls-17 {
+  stroke: #777;
+  stroke-width: 3px;
+}
+
+.cls-18, .cls-19 {
+  font-size: 9px;
+  opacity: 1;
+  transition: all 0.5s;
+}
+
+.cls-18, .cls-26 {
+  fill: #f3931f;
+}
+
+.cls-18, .cls-19, .cls-20, .cls-28 {
+  font-family: Roboto-Regular, Roboto;
+}
+
+.cls-19, .cls-27 {
+  fill: #41567e;
+}
+
+.cls-20, .cls-28 {
+  font-size: 6px;
+}
+
+.cls-20 {
+  fill: #1d1d1b;
+}
+
+.cls-21 {
+  letter-spacing: -0.01em;
+}
+
+.cls-22 {
+  letter-spacing: -0.02em;
+}
+
+.cls-23 {
+  letter-spacing: 0.01em;
+}
+
+.cls-24 {
+  letter-spacing: -0.01em;
+}
+
+.cls-25 {
+  letter-spacing: -0.01em;
+}
+
+.cls-28 {
+  fill: #cfcece;
+}
+
+.cls-27, .cls-26{
+  opacity: 1;
+  transition: all 0.5s;
+}
+
+
+svg{
+  width: 800px;
+  margin-top: -40px;
+}
+
+.button{
+  cursor: pointer;
+}
+
+.hide{
+  opacity: 0;
+}
+
+.disable{
+  fill: #e4eaea;
+}
+
 </style>
 
