8bcd9837732818aeace2b37087ba05e145f1754e
Moritz <moritz.banhardt@web.de>
2021-01-25 14:42:42 +0100

    info icon

--- ki-site/src/App.vue
+++ ki-site/src/App.vue
@@ -17,7 +17,15 @@
       <nav id="nav">
           <ul id="myMenu">
             <li data-menuanchor="Home" class="active"><a href="#Home"><svg fill="white" height="20px" viewBox="0 0 512 512" width="20px" xmlns="http://www.w3.org/2000/svg"><path d="m498.195312 222.695312c-.011718-.011718-.023437-.023437-.035156-.035156l-208.855468-208.847656c-8.902344-8.90625-20.738282-13.8125-33.328126-13.8125-12.589843 0-24.425781 4.902344-33.332031 13.808594l-208.746093 208.742187c-.070313.070313-.140626.144531-.210938.214844-18.28125 18.386719-18.25 48.21875.089844 66.558594 8.378906 8.382812 19.445312 13.238281 31.277344 13.746093.480468.046876.964843.070313 1.453124.070313h8.324219v153.699219c0 30.414062 24.746094 55.160156 55.167969 55.160156h81.710938c8.28125 0 15-6.714844 15-15v-120.5c0-13.878906 11.289062-25.167969 25.167968-25.167969h48.195313c13.878906 0 25.167969 11.289063 25.167969 25.167969v120.5c0 8.285156 6.714843 15 15 15h81.710937c30.421875 0 55.167969-24.746094 55.167969-55.160156v-153.699219h7.71875c12.585937 0 24.421875-4.902344 33.332031-13.808594 18.359375-18.371093 18.367187-48.253906.023437-66.636719zm0 0"/></svg></a></li>
-            <li data-menuanchor="Einfuehrung" class="active"><a href="#Einfuehrung"><img width="20px" height="20px" margin-top="7px" src="./assets/info.svg"></a></li>
+            <li data-menuanchor="Einfuehrung" class="active"><a href="#Einfuehrung"><svg class="info" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve"><g>
+              <g>
+                <path class="st0" d="M437,75C388.7,26.6,324.4,0,256,0S123.3,26.6,75,75C26.6,123.3,0,187.6,0,256s26.6,132.7,75,181
+                  c48.4,48.4,112.6,75,181,75s132.7-26.6,181-75c48.4-48.4,75-112.6,75-181S485.4,123.3,437,75z M256,70c30.3,0,55,24.7,55,55
+                  s-24.7,55-55,55s-55-24.7-55-55S225.7,70,256,70z M326,420H186v-30h30V240h-30v-30h110v180h30V420z"/>
+              </g>
+            </g>
+            </svg>
+            </a></li>
             <div class="section2">
               <li class="no-connection" data-menuanchor="AllgemeineBetrachtung"><a href="#AllgemeineBetrachtung">02</a>
                 <ul class="nav2 firstnav">
@@ -376,6 +384,10 @@ button:hover{
     fill:  #f3931f;
 }
 
+#nav .active a svg path{
+    fill:  #f3931f;
+}
+
 .fp-viewing-Funktionsbereich-3 #nav .thirdnav li:nth-child(4) .dot, .fp-viewing-Funktionsbereich-2 #nav .thirdnav li:nth-child(3) .dot, .fp-viewing-Funktionsbereich-1 #nav .thirdnav li:nth-child(2) .dot, .fp-viewing-Funktionsbereich-0 #nav .thirdnav li:nth-child(1) .dot{
   width: 8px;
   height: 8px;
@@ -433,4 +445,8 @@ button:hover{
   /* Farbe navi 04 */
   color: #C18BBF !important;
 }
+
+.st0{fill:#FFFFFF;}
+
+.info{width: 18px; margin-bottom: -3px;}
 </style>
